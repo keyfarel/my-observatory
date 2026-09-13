13320f14e014388552f462f6bb34b04a56336d89
keyfarel <carinsmddd@gmail.com>
2026-09-13 19:36:45 +0700

    feat(seo): add homepage and SEO component

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://keyfirdausi.dev',
+  site: 'https://keyfarel.my.id',
   integrations: [react(), mdx(), sitemap()],
 
   vite: {
